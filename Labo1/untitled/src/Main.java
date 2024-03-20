75a139e92e8a6ee0351e96728f067008d4cf1dca
EduardoSig2210 <EduardoSig2210>
2024-03-20 16:27:42 -0600

    Creando calculadora

--- Labo1/untitled/src/Main.java
+++ Labo1/untitled/src/Main.java
@@ -1,15 +1,76 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int opcion = 0, result = 0, num1 = 0, num2 = 0;
+         System.out.println("Bienvenino");
+         Scanner scanner = new Scanner(System.in);
+         while (opcion != 5) {
+             System.out.printf("1. Suma %n2. Resta %n3. Multiplicacion %n4. Division %n5. Salir %nQue desea realizar ");
+             opcion = scanner.nextInt();
+             switch (opcion) {
+                 case 1:
+                     //Suma
+                     System.out.println("Ingrese el primer numero: ");
+                     num1 = scanner.nextInt();
+                     System.out.println("Ingrese el segundo numero: ");
+                     num2 = scanner.nextInt();
+                     result = suma(num1,num2);
+                     System.out.println("La suma de " + num1 + " y " + num2  + " es igual a " + result);
+                     break;
+                 case 2:
+                    //Resta
+                     System.out.println("Ingrese el primer numero: ");
+                     num1 = scanner.nextInt();
+                     System.out.println("Ingrese el segundo numero: ");
+                     num2 = scanner.nextInt();
+                     result = resta(num1,num2);
+                     System.out.println("La resta de " + num1 + " y " + num2  + " es igual a " + result);
+                     break;
+                 case 3:
+                     //Multiplicacion
+                     System.out.println("Ingrese el primer numero: ");
+                     num1 = scanner.nextInt();
+                     System.out.println("Ingrese el segundo numero: ");
+                     num2 = scanner.nextInt();
+                     result = multiplicacion(num1,num2);
+                     System.out.println("La multiplicacion de " + num1 + " y " + num2  + " es igual a " + result);
+                     break;
+                 case 4:
+                     //Division
+                     System.out.println("Ingrese el primer numero: ");
+                     num1 = scanner.nextInt();
+                     System.out.println("Ingrese el segundo numero: ");
+                     num2 = scanner.nextInt();
+                     result = division(num1,num2);
+                     System.out.println("La division de " + num1 + " y " + num2  + " es igual a " + result);
+                     break;
+                 case 5:
+                     System.out.println("Tenga un buen dia");
+                    break;
+                 default:
+                     System.out.println("Opcion no valida");
+             }
+         }
+    }
+    public static int suma(int a, int b){
+        int total=0;
+        total=a+b;
+        return total;
+    }
+    public static int resta(int a, int b){
+        int total=0;
+        total=a-b;
+        return total;
+    }
+    public static int multiplicacion(int a, int b){
+        int total=0;
+        total=a*b;
+        return total;
+    }
+    public static int division(int a, int b){
+        int total=0;
+        total=a/b;
+        return total;
     }
 }
